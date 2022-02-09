b4e736c517cf2052fa1d59c8037d51f028d7b1bb
sohail bukhari <moiz.bukhari6@gmail.com>
2022-02-10 03:08:28 +0500

    make it better

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/login", indexRouter);
+app.use("/", indexRouter);
 app.use("/products", productsRouter);
-app.use("/login", usersRouter);
+app.use("/", usersRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
