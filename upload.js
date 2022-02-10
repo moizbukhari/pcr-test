49ccc5c0f8899a6af040358fc74c2402f6f0914b
sohail bukhari <moiz.bukhari6@gmail.com>
2022-02-10 11:37:43 +0500

    make it better

--- upload.js
+++ upload.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const maxSize = 2 * 1024 * 1024;
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, `${__basedir}/public/`);
+    cb(null, `public/stylesheets/`);
   },
   filename: (req, file, cb) => {
     console.log(file.originalname);
