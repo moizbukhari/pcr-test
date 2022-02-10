23f58b3b4a1b7de678d59242b01db6725e281fba
sohail bukhari <moiz.bukhari6@gmail.com>
2022-02-10 09:08:59 +0500

    make it better

--- routes/products.js
+++ routes/products.js
@@ -85,19 +85,19 @@ router.get("/edit/:id", async function (req, res, next) {
 // };
 
 router.post("/edit/:id", async function (req, res, next) {
-  try {
-    await uploadFile(req, res);
-    if (req.file == undefined) {
-      return res.status(400).send({ message: "Please upload a file!" });
-    }
-    res.status(200).send({
-      message: "Uploaded the file successfully: " + req.file.originalname,
-    });
-  } catch (err) {
-    res.status(500).send({
-      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
-    });
-  }
+  // try {
+  //   await uploadFile(req, res);
+  //   if (req.file == undefined) {
+  //     return res.status(400).send({ message: "Please upload a file!" });
+  //   }
+  //   res.status(200).send({
+  //     message: "Uploaded the file successfully: " + req.file.originalname,
+  //   });
+  // } catch (err) {
+  //   res.status(500).send({
+  //     message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+  //   });
+  // }
 
 
   let product = await Product.findById(req.params.id);
